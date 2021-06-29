749a747754648b97165583c253a1891f8eda8da2
Dhruv Kumar Patwari <dhruv.patwari@zemosolabs.com>
2021-06-29 18:09:22 +0530

    Question 1 solved by removing the duplicate const decleration

--- quick-chat-debug/public/app.js
+++ quick-chat-debug/public/app.js
@@ -75,7 +75,6 @@ sendBtn.addEventListener('click', e => {
 	const day = date.getDate();
 	const year = date.getFullYear();
 	const dateString = `${month}/${day}/${year}`;
-	const dateString = `${month}/${day}`;
 	
 
 
--- quick-chat-debug/server.js
+++ quick-chat-debug/server.js
@@ -25,6 +25,6 @@ io.on('connection', socket => {
 	});
 });
 
-http.listen(3000, () => {
-	console.log('listening on port 3000');
+http.listen(3001, () => {
+	console.log('listening on port 3001');
 });
